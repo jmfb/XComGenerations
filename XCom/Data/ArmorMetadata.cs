8096c9a3cae7319acd6697cbfaa028fed76b94ae
Jacob Buysse <Jacob.Buysse@gmail.com>
2015-08-18 18:20:41 -0500

    Adding armor metadata.

--- XCom/Data/ArmorMetadata.cs
+++ XCom/Data/ArmorMetadata.cs
@@ -3,5 +3,13 @@
 	public class ArmorMetadata
 	{
 		public string Name { get; set; }
+		public int FrontArmor { get; set; }
+		public int LeftArmor { get; set; }
+		public int RightArmor { get; set; }
+		public int RearArmor { get; set; }
+		public int UnderArmor { get; set; }
+		public bool FireResistant { get; set; }
+		public bool SmokeResistant { get; set; }
+		public int StunResistance { get; set; }
 	}
 }
--- XCom/Data/ArmorType.cs
+++ XCom/Data/ArmorType.cs
@@ -4,6 +4,7 @@ namespace XCom.Data
 {
 	public enum ArmorType
 	{
+		Coveralls,
 		PersonalArmor,
 		PowerSuit,
 		FlyingSuit
@@ -16,23 +17,57 @@ namespace XCom.Data
 			return metadata[armorType];
 		}
 
+		private static readonly ArmorMetadata coveralls = new ArmorMetadata
+		{
+			Name = "Coveralls",
+			FrontArmor = 12,
+			LeftArmor = 8,
+			RightArmor = 8,
+			RearArmor = 5,
+			UnderArmor = 2
+		};
+
 		private static readonly ArmorMetadata personalArmor = new ArmorMetadata
 		{
-			Name = "PERSONAL ARMOR"
+			Name = "PERSONAL ARMOR",
+			FrontArmor = 50,
+			LeftArmor = 40,
+			RightArmor = 40,
+			RearArmor = 30,
+			UnderArmor = 30,
+			FireResistant = true,
+			StunResistance = 10
 		};
 
 		private static readonly ArmorMetadata powerSuit = new ArmorMetadata
 		{
-			Name = "POWER SUIT"
+			Name = "POWER SUIT",
+			FrontArmor = 100,
+			LeftArmor = 80,
+			RightArmor = 80,
+			RearArmor = 70,
+			UnderArmor = 60,
+			FireResistant = true,
+			SmokeResistant = true,
+			StunResistance = 20
 		};
 
 		private static readonly ArmorMetadata flyingSuit = new ArmorMetadata
 		{
-			Name = "FLYING SUIT"
+			Name = "FLYING SUIT",
+			FrontArmor = 110,
+			LeftArmor = 90,
+			RightArmor = 90,
+			RearArmor = 80,
+			UnderArmor = 70,
+			FireResistant = true,
+			SmokeResistant = true,
+			StunResistance = 20
 		};
 
 		private static readonly Dictionary<ArmorType, ArmorMetadata> metadata = new Dictionary<ArmorType,ArmorMetadata>
 		{
+			{ ArmorType.Coveralls, coveralls },
 			{ ArmorType.PersonalArmor, personalArmor },
 			{ ArmorType.PowerSuit, powerSuit },
 			{ ArmorType.FlyingSuit, flyingSuit }
