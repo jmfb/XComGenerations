1e72171c28c995205861ea60e132c5013a5bce6d
Jacob Buysse <Jacob.Buysse@gmail.com>
2015-09-19 22:14:52 -0500

    Replacing terrible triangle fill algorithm (fixed rendering glitches).

--- XCom/Graphics/GraphicsBuffer.cs
+++ XCom/Graphics/GraphicsBuffer.cs
@@ -11,15 +11,13 @@ namespace XCom.Graphics
 		public const int GameWidth = 320;
 		public const int GameHeight = 200;
 		public const int BytesPerPixel = 3;
-		private const int BytesPerRow = GameWidth * BytesPerPixel;
-		private const int ByteCount = BytesPerRow * GameHeight;
-		private readonly byte[] data = new byte[ByteCount];
-
-		public byte[] Buffer => data;
+		private const int bytesPerRow = GameWidth * BytesPerPixel;
+		private const int byteCount = bytesPerRow * GameHeight;
+		public byte[] Buffer { get; } = new byte[byteCount];
 
 		public void Clear()
 		{
-			Array.Clear(data, 0, data.Length);
+			Array.Clear(Buffer, 0, Buffer.Length);
 		}
 
 		public void DrawHorizontalLine(
@@ -89,11 +87,11 @@ namespace XCom.Graphics
 			//OpenGL buffer has row 0 at the bottom
 			//Row is inverted for normal window coordinates (0 at top).
 			var invertedRow = GameHeight - row - 1;
-			var index = invertedRow * BytesPerRow + column * BytesPerPixel;
+			var index = invertedRow * bytesPerRow + column * BytesPerPixel;
 			var function = GetPixelFunction(operation);
 			var source = new[] { color.R, color.G, color.B };
 			foreach (var part in Enumerable.Range(0, BytesPerPixel))
-				data[index + part] = function(source[part], data[index + part]);
+				Buffer[index + part] = function(source[part], Buffer[index + part]);
 		}
 
 		public void DrawBackground(
@@ -137,87 +135,80 @@ namespace XCom.Graphics
 				paletteIndex);
 		}
 
-		//TODO: implement this completely differently
+		private static void Swap<T>(ref T value1, ref T value2)
+		{
+			var temp = value1;
+			value1 = value2;
+			value2 = temp;
+		}
+
+		private static IEnumerable<Point> Line(Point from, Point to)
+		{
+			var x0 = from.X;
+			var y0 = from.Y;
+			var x1 = to.X;
+			var y1 = to.Y;
+			var steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+			if (steep)
+			{
+				Swap(ref x0, ref y0);
+				Swap(ref x1, ref y1);
+			}
+			if (x0 > x1)
+			{
+				Swap(ref x0, ref x1);
+				Swap(ref y0, ref y1);
+			}
+			var dX = x1 - x0;
+			var dY = Math.Abs(y1 - y0);
+			var error = dX / 2;
+			var yStep = y0 < y1 ? 1 : -1;
+			var y = y0;
+
+			for (var x = x0; x <= x1; ++x)
+			{
+				yield return steep ?
+					new Point { X = y, Y = x } :
+					new Point { X = x, Y = y };
+				error = error - dY;
+				if (error >= 0)
+					continue;
+				y += yStep;
+				error += dX;
+			}
+		}
+
+		private static IEnumerable<Point> Triangle(Point[] vertices)
+		{
+			foreach (var group in Line(vertices[0], vertices[1])
+				.Concat(Line(vertices[1], vertices[2]))
+				.Concat(Line(vertices[2], vertices[0]))
+				.GroupBy(point => point.Y)
+				.Where(group => group.Key >= 0 && group.Key < GameHeight))
+			{
+				var minX = group.Min(point => point.X);
+				var maxX = group.Max(point => point.X);
+				foreach (var column in Enumerable.Range(minX, maxX - minX + 1)
+					.Where(column => column >= 0 && column < 256))
+				{
+					yield return new Point { X = column, Y = group.Key };
+				}
+			}
+		}
+
 		public void DrawTerrain(
 			Terrain terrain,
 			int shading, //0-8
 			int zoom) //0-5
 		{
 			var mask = terrain.TerrainType.Metadata().Image(zoom);
-			DrawTerrainTriangle(mask, shading, terrain.Vertices);
-		}
-
-		private void DrawTerrainTriangle(byte[] mask, int shading, Point[] vertices)
-		{
-			//http://www.sunshine2k.de/coding/java/Bresenham/RasterisingLinesCircles.pdf
-			//http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html#algo2
-			var sortedVertices = vertices.OrderBy(vertex => vertex.Y).ToArray();
-			if (sortedVertices[1].Y == sortedVertices[2].Y)
-			{
-				DrawTerrainTriangleFlatBottom(mask, shading, sortedVertices);
-			}
-			else if (sortedVertices[0].Y == sortedVertices[1].Y)
-			{
-				DrawTerrainTriangleFlatTop(mask, shading, sortedVertices);
-			}
-			else
-			{
-				var y10 = (float)(sortedVertices[1].Y - sortedVertices[0].Y);
-				var y20 = (float)(sortedVertices[2].Y - sortedVertices[0].Y);
-				var x20 = (float)(sortedVertices[2].X - sortedVertices[0].X);
-				var midVertex = new Point
-				{
-					X = (int)(sortedVertices[0].X + (y10 /  y20) * x20),
-					Y = sortedVertices[1].Y
-				};
-				DrawTerrainTriangleFlatBottom(mask, shading, new[] { sortedVertices[0], sortedVertices[1], midVertex });
-				DrawTerrainTriangleFlatTop(mask, shading, new[] { sortedVertices[1], midVertex, sortedVertices[2] });
-			}
-		}
-
-		private void DrawTerrainTriangleFlatBottom(byte[] mask, int shading, Point[] vertices)
-		{
-			var inverseSlope1 = (float)(vertices[1].X - vertices[0].X) / (vertices[1].Y - vertices[0].Y);
-			var inverseSlope2 = (float)(vertices[2].X - vertices[0].X) / (vertices[2].Y - vertices[0].Y);
-			var currentX1 = (float)vertices[0].X;
-			var currentX2 = (float)vertices[0].X;
-			foreach (var row in Enumerable.Range(vertices[0].Y, vertices[1].Y - vertices[0].Y + 1))
-			{
-				var x1 = (int)Math.Min(currentX1, currentX2);
-				var x2 = (int)Math.Max(currentX1, currentX2);
-				DrawTerrainLine(row, x1, x2 - x1 + 1, mask, shading);
-				currentX1 += inverseSlope1;
-				currentX2 += inverseSlope2;
-			}
-		}
-
-		private void DrawTerrainTriangleFlatTop(byte[] mask, int shading, Point[] vertices)
-		{
-			var inverseSlope1 = (float)(vertices[2].X - vertices[0].X) / (vertices[2].Y - vertices[0].Y);
-			var inverseSlope2 = (float)(vertices[2].X - vertices[1].X) / (vertices[2].Y - vertices[1].Y);
-			var currentX1 = (float)vertices[2].X;
-			var currentX2 = (float)vertices[2].X;
-			foreach (var row in Enumerable.Range(0, vertices[2].Y - vertices[0].Y + 1).Select(index => vertices[2].Y - index))
-			{
-				currentX1 -= inverseSlope1;
-				currentX2 -= inverseSlope2;
-				var x1 = (int)Math.Min(currentX1, currentX2);
-				var x2 = (int)Math.Max(currentX1, currentX2);
-				DrawTerrainLine(row, x1, x2 - x1 + 1, mask, shading);
-			}
-		}
-
-		private void DrawTerrainLine(int row, int leftColumn, int width, byte[] mask, int shading)
-		{
-			if (row < 0 || row >= GameHeight)
-				return;
 			var palette = Palette.GetPalette(0);
-			var maskRow = (row % 32 + 32) % 32;
-			foreach (var column in Enumerable.Range(leftColumn, width).Where(column => column >= 0 && column < 256))
+			foreach (var point in Triangle(terrain.Vertices))
 			{
-				var maskColumn = (column % 32 + 32) % 32;
+				var maskRow = (point.Y % 32 + 32) % 32;
+				var maskColumn = (point.X % 32 + 32) % 32;
 				var maskIndex = maskRow * 32 + maskColumn;
-				SetPixel(row, column, palette.GetColor(mask[maskIndex] + shading));
+				SetPixel(point.Y, point.X, palette.GetColor(mask[maskIndex] + shading));
 			}
 		}
 
